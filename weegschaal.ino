3d621ee7c1fe8afad3d80fedf551ab7b481a1bad
drevil66635 <annesteenbeek@gmail.com>
2014-07-31 00:48:57 +0200

    dit is nieuw

--- weegschaal.ino
+++ weegschaal.ino
@@ -6,24 +6,25 @@
  This example code is in the public domain.
  */
 
+float voltageAverage = 2.5;
+
+float calibrateMassA = 0.0;
+float calibrateVoltageA = 2.7;
+
+float calibrateMassB = 65.0;
+float calibrateVoltageB = 4.16;
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-}
-
-float voltageAverage = 2.5;
-int calibrateValueA = 2.7;
-float calibrateMassA = 0.0;
-float calibrateVoltageA = calibrateValueA * (5.0 / 1023.0);
-int calibrateValueB = 4.16;
-float calibrateVoltageB = calibrateValueB * (5.0 / 1023.0);
-float calibrateMassB = 65;
 
 Serial.print("Calibrate voltage A is: "); Serial.print(calibrateVoltageA); Serial.print("\t");
 Serial.print("Calibrate mass A is: "); Serial.println(calibrateMassA);
 Serial.print("Calibrate voltage B is: "); Serial.print(calibrateVoltageB); Serial.print("\t");
 Serial.print("Calibrate mass B is: "); Serial.println(calibrateMassB);
+}
+
+
 
 // the loop routine runs over and over again forever:
 void loop() {
@@ -35,8 +36,7 @@ void loop() {
   voltageAverage = (0.90*voltageAverage + 0.10*voltage);
   
   // print out the value you read:
-    Serial.print("Voltage is: "); Serial.print(voltage); Serial.print("\t");
+  Serial.print("Voltage is: "); Serial.print(voltage); Serial.print("\t");
   Serial.print("sensorValue is: "); Serial.print(sensorValue); Serial.print("\t");
   Serial.print("voltageAverage is: "); Serial.println(voltageAverage);
 }
-
