bd7c734934f257ea0e922b96cb34177dc0ef49ff
meerarjen <meerarjen@gmail.com>
2014-08-03 01:49:11 +0200

    hanmatige reset ingebouwd
    als je reset intypt in de serial viewer dan wordt het startvoltage
    opnieuw berekend.
    Om de  zoveel tijd reset hij zichzelf spontaan. Ben er nog niet achter
    waarom dat precies gebeurt...

--- weegschaal.ino
+++ weegschaal.ino
@@ -1,7 +1,7 @@
 /*
   ReadAnalogVoltage
-  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+ Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
+ Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
  
  This example code is in the public domain.
  */
@@ -18,16 +18,14 @@ void setup() {
   Serial.begin(9600);
 
   timer = micros();
-
-while( micros()-timer < 100 ) { // dit stukje 100microsec laten draaien om gemiddelde te krijgen
-  i++;
-  startVolt+=(analogRead(A0)* (5.0 / 1023.0))/i;
+  while( micros()-timer < 100 ) { // dit stukje 100microsec laten draaien om gemiddelde te krijgen
+    i++;
+    startVolt+=(analogRead(A0)* (5.0 / 1023.0))/i;
+  }
+  Serial.print("Start Voltage is: "); 
+  Serial.println(startVolt);
+  i=0;
 }
-Serial.print("Start Voltage is: "); Serial.print(startVolt);
-
-}
-
-
 
 // the loop routine runs over and over again forever:
 void loop() {
@@ -38,15 +36,45 @@ void loop() {
   //smooth out with average voltage
   voltageAverage = (0.90*voltageAverage + 0.10*voltage);
   float x = analogToLoad(voltageAverage);
-  
-  Serial.print("Gewicht is: "); Serial.print(x); Serial.print("\t"); Serial.print(voltageAverage); Serial.print("\t"); Serial.println(voltweight);
+
+  Serial.print("Gewicht is: "); 
+  Serial.print(x); 
+  Serial.print("\t"); 
+  Serial.print(voltageAverage); 
+  Serial.print("\t"); 
+  Serial.println(voltweight);
+
   // hier kun je data naar de serial port sturen
-  if (Serial.available() > 0) {
-    weight = (float) Serial.parseInt();
-    voltweight=voltageAverage;
-    Serial.print("het huidige gewicht is: "); Serial.println(weight);
+  int j=0;
+  char buffer[100];
+  while (Serial.available() && j<99) {
+    buffer[j++] = Serial.read();
   }
+  buffer[i++]='\0';
+  if(j>0){
+    Serial.println((char*)buffer);
+    if(strcmp(buffer, "reset") == 0){
+      Serial.println("Het wordt gereset!");
+      timer = micros();
+      while( micros()-timer < 100 ) { // dit stukje 100microsec laten draaien om gemiddelde te krijgen
+        i++;
+        startVolt+=(analogRead(A0)* (5.0 / 1023.0))/i;
+      }
+      i=0;
+      Serial.print("Start Voltage is: "); 
+      Serial.println(startVolt);
+    }
+    else{      
+      weight = atof(buffer);
+      voltweight=voltageAverage;
+      Serial.print("het huidige gewicht is: "); 
+      Serial.println(weight);
+    }
+  }
+  delay(200);
 }
+
+//functies:
 
 float analogToLoad(float analogval){
 
@@ -59,3 +87,6 @@ float mapfloat(float x, float in_min, float in_max, float out_min, float out_max
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+
+
